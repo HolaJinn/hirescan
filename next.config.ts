5935bf0f51c56519572955edecb98c9757530756
Yacine Ben Amor <benamor.yacine@yahoo.fr>
2025-09-24 18:06:34 +0100

    modified next.config.ts to fix the pdf-parse error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['pdf-parse'],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
